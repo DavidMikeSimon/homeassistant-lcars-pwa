3e0647e258bb70a2373b9a55dc9e02fe9be1f5c2
David Simon <david.simon@color.com>
2023-02-04 12:24:12 -0800

    connect to server

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -1,17 +1,48 @@
-import { h } from 'preact';
-import { Route, Router } from 'preact-router';
+import { h } from "preact";
+import { Route, Router } from "preact-router";
+import {
+  getAuth,
+  createConnection,
+  subscribeEntities,
+  ERR_HASS_HOST_REQUIRED,
+} from "home-assistant-js-websocket";
 
-// Code-splitting is automated for `routes` directory
-import Home from '../routes/home';
+async function connect() {
+  let auth;
+  try {
+    // Try to pick up authentication after user logs in
+    auth = await getAuth();
+  } catch (err) {
+    if (err === ERR_HASS_HOST_REQUIRED) {
+      const hassUrl = prompt(
+        "What host to connect to?",
+        "https://sinclair-ha.pipsimon.com"
+      );
+      // Redirect user to log in on their instance
+      auth = await getAuth({ hassUrl });
+    } else {
+      alert(`Unknown error: ${err}`);
+      return;
+    }
+  }
+  const connection = await createConnection({ auth });
+  subscribeEntities(connection, (ent) => console.log(ent));
+}
 
-const App = () => (
-	<div id="app">
-		<main>
-			<Router>
-				<Route path="/" component={Home} />
-			</Router>
-		</main>
-	</div>
-);
+import Home from "../routes/home";
+
+const App = () => {
+  connect();
+
+  return (
+    <div id="app">
+      <main>
+        <Router>
+          <Route path="/" component={Home} />
+        </Router>
+      </main>
+    </div>
+  );
+};
 
 export default App;
